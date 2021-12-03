25296547d317c217cf6653ea2143ba784110ea4a
zigster64 <steve@raffmail.net>
2021-12-03 14:08:40 +1000

    added PHP from kirana, because lazy

---
+++ php/day2/day2.php
@@ -0,0 +1,13 @@
+<?php
+   $file = file('input.data');
+   $pos = $dep = 0;
+   foreach ($file as $row) {
+     [$dir, $val] = sscanf($row, '%s %i');
+     [$pos, $dep] = match ($dir) {
+        'up'      => [$pos, $dep - $val],
+        'down'    => [$pos, $dep + $val],
+        'forward' => [$pos + $val, $dep],
+     };
+   }
+   echo "pos: $pos; dep: $dep; result: " . ($pos * $dep) . "\n";
+
